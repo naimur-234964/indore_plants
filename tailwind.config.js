e3d2db13318e0c553cd49ab09624988f1dda7093
Naimur Rahman <nrsazib234964@gmail.com>
2024-10-30 22:36:04 +0600

    set environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['*'],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+      xxl: "1480px",
+      xxxl: "1780px"
+    },
     extend: {},
+    fontFamily:{
+      Jost:["Jost", "sans-serif"],
+      Lobster:["Lobster", "sans-serif"]
+    },
+    container: {
+      center: true,
+      padding:{
+        DEFAULT: "12px",
+        md: "32px"
+      }
+    }
   },
   plugins: [],
 }
