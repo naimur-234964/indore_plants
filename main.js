520d35bf4080a14534210bc4c24349014238d0a7
Naimur Rahman <nrsazib234964@gmail.com>
2024-11-11 13:27:44 +0600

    navLink active added

--- main.js
+++ main.js
@@ -42,3 +42,30 @@ const scrollHeader = () => {
 }
 
 window.addEventListener("scroll", scrollHeader)
+
+// Active Link
+
+const activeLink = () => {
+    const sections = document.querySelectorAll("section")
+    const navLinks = document.querySelectorAll(".nav-link")
+
+    let current = "home"
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop
+
+        if(this.scrollY >= sectionTop - 60){
+            current = section.getAttribute("id")
+        }
+    })
+
+    navLinks.forEach(item  => {
+        item.classList.remove("active")
+        
+        if(item.href.includes(current)){
+            item.classList.add("active")
+        }
+    })
+}
+
+window.addEventListener("scroll", activeLink)
