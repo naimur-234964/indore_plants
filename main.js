3feef4606318c5743b4f0eb0527bcd896e8d5dc3
Naimur Rahman <nrsazib234964@gmail.com>
2024-11-11 14:42:01 +0600

    scroll reveal completed

--- main.js
+++ main.js
@@ -35,9 +35,9 @@ const scrollHeader = () => {
     const header = document.getElementById("navbar")
 
     if (this.scrollY >= 250) {
-        header.classList.add("border-b","border-yellow-500")
+        header.classList.add("border-b", "border-yellow-500")
     } else {
-        header.classList.remove("border-b","border-yellow-500")
+        header.classList.remove("border-b", "border-yellow-500")
     }
 }
 
@@ -52,20 +52,40 @@ const activeLink = () => {
     let current = "home"
 
     sections.forEach(section => {
-        const sectionTop = section.offsetTop
+        const sectionTop = section.offsetTop -60
 
-        if(this.scrollY >= sectionTop - 60){
+        if (this.scrollY >= sectionTop - 60) {
             current = section.getAttribute("id")
         }
     })
 
-    navLinks.forEach(item  => {
+    navLinks.forEach(item => {
         item.classList.remove("active")
-        
-        if(item.href.includes(current)){
+
+        if (item.href.includes(current)) {
             item.classList.add("active")
         }
     })
 }
 
 window.addEventListener("scroll", activeLink)
+
+// scroll reveal animation
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 30,
+    reset: true
+})
+
+sr.reveal(".home_data, .about_top, .popular_top, .review_top, .review_swiper, .footer_icon, .footer_content, footer_floral, .copyright")
+sr.reveal(".home_image", { delay: 500, scale: 0.5 })
+
+sr.reveal(".service_card, .popular_card", { interval: 100 })
+
+sr.reveal(".about_leaf", { delay: 1000, origin: "right" })
+sr.reveal(".about_card_1-img,.about_card_2-content ", { origin: "left" })
+sr.reveal(".about_card_2-img, .about_card_1-content", { origin: "right" })
+
+sr.reveal(".review_leaf, .footer_floral", { delay: 1000, origin: "left" })
