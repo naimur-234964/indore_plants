b0f823f3ce3a72ed7f400e1aaecbafc0c1645104
Naimur Rahman <nrsazib234964@gmail.com>
2024-11-11 12:57:16 +0600

    scrollUp button added

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ navLink.forEach(link => {
         hamburger.classList.toggle("ri-close-large-line")
     })
 })
+
+
+// Show Scroll Up
+
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up")
+
+    if (this.scrollY >= 250){
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    }else {
+        scrollUpBtn.classList.add("-bottom-1/2")
+        scrollUpBtn.classList.remove("bottom-4")
+    }
+}
+
+window.addEventListener("scroll", scrollUp)
